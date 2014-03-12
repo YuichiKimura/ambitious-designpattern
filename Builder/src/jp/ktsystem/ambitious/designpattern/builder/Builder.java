d056b84431e01e8cd5907b59b7e21bc58e2d23b6
yuichi <yuichi.kimura1130@gmail.com>
2014-03-12 21:32:11 +0900

    コミット

--- Builder/src/jp/ktsystem/ambitious/designpattern/builder/Builder.java
+++ Builder/src/jp/ktsystem/ambitious/designpattern/builder/Builder.java
@@ -16,12 +16,11 @@ public class Builder {
 	// --- public
 
 	public Builder add(String s) {
-		
-		// 変更を加えました。
-		// 2014/03/12 変更
-		// ブランチを切って変更
-		// 再度変更
-		
+
+		// 螟画峩繧貞刈縺医∪縺励◆縲�		// 2014/03/12 螟画峩
+		// 繝悶Λ繝ｳ繝√ｒ蛻�▲縺ｦ螟画峩
+		// 蜀榊ｺｦ螟画峩
+
 		if (!initialized) throw new RuntimeException("Builder is not initialized.");
 		this.s += s; // (Better to use StringBuilder.)
 		return this;
